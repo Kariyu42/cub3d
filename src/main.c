206bfaf7a2f070fa8c5cae513b816b7ecb72f293
Kariyu42 <rioukian01@gmail.com>
2023-11-14 12:52:16 +0100

    📝✨ added checklist

--- src/main.c
+++ src/main.c
@@ -1,6 +1,28 @@
 #include "cub3d.h"
 #include "err_type.h"
 
+/*
+* CheckList for the Map
+TODO check extension .cub ✅
+TODO collect map, trim \n ✅
+TODO Check map characters
+TODO At the same time check at least for ONLY one player, Not more than one.
+TODO Surround (entouré la carte) map with '\' ou pas... juste les espaces.
+TODO Check map is closed using flood fill
+! Need to check if there is more to add...
+
+* CheckList for the Data ABOVE the map.
+TODO collect data, STOP condition is: if we encouter map we stop parsing.
+TODO check if all data is present.
+TODO check if path of texture is valid.
+TODO check format of data SO, WE, NO, EA, C, F.
+TODO color: check that colors are separated by colons ",".
+TODO color: check that integer doesn't exceed 0 - 255.
+
+* OMG RayCasting...
+
+*/
+
 int	main(int ac, char **av)
 {
 	t_config	*conf;
